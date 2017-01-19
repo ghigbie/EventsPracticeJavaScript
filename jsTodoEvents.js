22670fbb72c8993fdc5117f23a2c12b98a81bacb
George Higbie <gehigbie@gmail.com>
2017-01-19 16:09:08 -0700

    began some custom functions

--- jsTodoEvents.js
+++ jsTodoEvents.js
@@ -2,5 +2,36 @@
 var lis = document.querySelectorAll("li");
 
 for(var i = 0; i < lis.length; i++){
-	
+	lis[i].addEventListener("click", function(){
+		lis[i]
+	} );
 }
+
+function clickListener(item){
+	item.addEventListener("click", )
+}
+
+function doubleClick(item){
+	item.classList.add("");
+}
+
+// .hover{
+// 	color: #4286f4;
+
+// }
+
+// .click{
+// 	color: #f442b9;
+
+// }
+
+// .clicked{
+// 	color: #e50d0d;
+// 	text-decoration: line-through;
+
+// }
+
+// .doubleClicked{
+// 	color: #afe50d;
+// 	text-decoration: line-through;
+// }

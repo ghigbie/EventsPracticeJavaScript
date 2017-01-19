7ebaac75fb6e419450c7970583338e7ac4201b7c
George Higbie <gehigbie@gmail.com>
2017-01-19 16:34:13 -0700

    created more variables

--- jsTodoEvents.js
+++ jsTodoEvents.js
@@ -1,24 +1,32 @@
 //this document was created in the command line
 alert('JS file connected');
 var lis = document.querySelectorAll("li");
+var firstLI = document.querySelector("li");
+var secondLI = document.querySelectorAll("li")[1];
+var thirdLI = document.querySelecctorALL("li")[2];
 
-for(var i = 0; i < lis.length; i++){
-	lis[i].addEventListener("click", clickListener(lis[i]));
-}
+firstLI.addEventListener("mouseover", function(){
 
-function clickListener(item){
-	item.classList.add("click");
-}
+});
 
-function doubleClick(item){
-	item.classList.add("dooubleCliced");
-}
 
-function hoverOn(item){
-	item.classList.add("hover");
-}
+// for(var i = 0; i < lis.length; i++){
+// 	lis[i].addEventListener("click", clickListener(lis[i]));
+// }
 
-function clickedOn(item){
-	item.classList.add("clicked");
+// function clickListener(item){
+// 	item.classList.add("click");
+// }
+
+// function doubleClick(item){
+// 	item.classList.add("dooubleCliced");
+// }
+
+// function hoverOn(item){
+// 	item.classList.add("hover");
+// }
+
+// function clickedOn(item){
+// 	item.classList.add("clicked");
 }
 
